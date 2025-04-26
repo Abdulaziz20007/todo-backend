21a6161aed20f0f778e376e4e3537526e72240c6
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-04-26 19:22:06 +0500

    done

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3001;
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
+
   await app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
